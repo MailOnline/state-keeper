e5bcb09265532d41a3bd97c8f2c19988f16bbb94
Luis Silva <luis.dasilva@mailonline.co.uk>
2016-02-11 17:48:47 +0000

    [Luis] add browserify to state-keeper

--- src/subject.js
+++ src/subject.js
@@ -1,57 +1,47 @@
-(function (){
+var Subject = function (bindMethod, unbindMethod){
+  var cbs = {};
+  bindMethod = bindMethod || 'on';
+  unbindMethod = unbindMethod || 'off';
 
-  var Subject = function (bindMethod, unbindMethod){
-    var cbs = {};
+  var out = {};
 
-    bindMethod = bindMethod || 'on';
-    unbindMethod = unbindMethod || 'off';
-
-    var out = {};
-
-    out[bindMethod] = function (type, cb){
-      if (type in cbs){
-        cbs[type].push(cb);
-      }
-      else {
-        cbs[type] = [cb];
-      }
-    };
-
-    out[unbindMethod] = function (type, func){
-      var i, out;
-      if (type in cbs){
-        if (typeof func == "undefined"){
-          delete cbs[type];
-        }
-        else {
-          out = [];
-          for (i = 0;i < cbs[type].length ; i++){
-            if (cbs[type][i] !== func){
-              out.push(cbs[type][i]);
-            }
-          }
-          cbs[type] = out;
-        }
-      }
-    };
-
-    out.trigger = function (type, evt){
-      var i;
-      if (type in cbs){
-        for (i = 0;i < cbs[type].length ; i++){
-          cbs[type][i].call(this, evt);
-        }
-      }
-    };
-
-    return out;
+  out[bindMethod] = function (type, cb){
+    if (type in cbs){
+      cbs[type].push(cb);
+    }
+    else {
+      cbs[type] = [cb];
+    }
   };
 
-  if (typeof exports === 'object'){
-      module.exports = Subject;
-  }
-  else if (typeof window === 'object'){
-      window.StateKeeper.Subject = Subject;
-  }
+  out[unbindMethod] = function (type, func){
+    var i, out;
+    if (type in cbs){
+      if (typeof func == "undefined"){
+        delete cbs[type];
+      }
+      else {
+        out = [];
+        for (i = 0;i < cbs[type].length ; i++){
+          if (cbs[type][i] !== func){
+            out.push(cbs[type][i]);
+          }
+        }
+        cbs[type] = out;
+      }
+    }
+  };
 
-}());
+  out.trigger = function (type, evt){
+    var i;
+    if (type in cbs){
+      for (i = 0;i < cbs[type].length ; i++){
+        cbs[type][i].call(this, evt);
+      }
+    }
+  };
+
+  return out;
+};
+
+module.exports = Subject;
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+"use strict";
+
+require('setImmediate');
+var root = (global === 'undefined' ? window : global);
+
+function isString(s){
+  return (typeof s === "string" ) || (s instanceof String);
+}
+
+module.exports = {
+  isString: isString,
+  setImmediate: root.setImmediate
+};
